d631ba4f6552be907fd8dd85ae1da748cd60282f
Najmul11 <najmulbinnurul@gmail.com>
2023-06-19 08:39:03 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-import config from "./config/index";
+import config from './config/index'
 
-const app: Application = express();
+const app: Application = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (req: Request, res: Response) => {
+  res.send(`Server running on ${config.port}`)
+})
 
-app.get('/', (req: Request, res: Response)=>{
-    res.send(`Server running on ${config.port}`)
-} )
-
-export default app ; 
+export default app
+// helloo
